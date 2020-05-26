a82b662c6c69f9a73a5026029f01381b67429e2d
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 13:27:06 +0530

    Controller added

--- Redis/src/main/java/com/spring/Redis/controller/RedisController.java
+++ Redis/src/main/java/com/spring/Redis/controller/RedisController.java
@@ -1,5 +1,10 @@
 package com.spring.Redis.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/rest/url")
+@RestController
 public class RedisController {
 
 }
